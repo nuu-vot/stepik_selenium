272fd514d4212724b21073e5999d72390af1b198
Pavel <nuu.vot@gmail.com>
2022-11-16 19:53:50 +0300

    update page object

--- part_6/pages/base_page.py
+++ part_6/pages/base_page.py
@@ -1,14 +1,23 @@
 """Объект страницы"""
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage():
     """Класс для работы с базовой страницей"""
 
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         """Конструктор"""
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         """Метод для открытия страницы"""
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
--- part_6/pages/main_page.py
+++ part_6/pages/main_page.py
@@ -8,6 +8,10 @@ class MainPage(BasePage):
     """Класс для работы с главной страницей"""
 
     def go_to_login_page(self):
-        """Проверка ссылки на страницу логина"""
+        """Проверка перехода на страницу логина"""
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        """Проверка наличия ссылки на страницу логина"""
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
